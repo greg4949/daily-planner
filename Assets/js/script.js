e9ce73a3101c762bad6f4900344adb68af9b04a4
WJFil <greg.kitchen7@gmail.com>
2023-01-14 16:16:29 -0700

    for loop to format time block colors based on whether time block is in past present future

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,44 @@
 
 var calendarDate = $('#currentDay');
-var currentDate = dayjs().format('MMMM d, YYYY')
+var currentDate = dayjs().format('MMMM d, YYYY');
+calendarDate.text(currentDate);
 
-console.log(currentDate)
-calendarDate.text(currentDate)
+
 
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
 $(function () {
+  var calendarDate = $('#currentDay');
+  var currentDate = dayjs().format('MMMM d, YYYY');  
+  var timeBlockArr=['09','10','11','12','13','14','15','16','17']
+  var currentHour = dayjs().format('HH');
+  var currentHourText = 'hour-'+currentHour;
+
+  //set date at top of calendar to current date
+  calendarDate.text(currentDate);
+
+  //loop through each time block element and set color based on if time block hour is before, equal to or after current hour
+  for (var i=0; i < timeBlockArr.length; i++){
+
+    var block = $('#hour-'+timeBlockArr[i]);
+    var blockHourText = block.attr('id');
+
+
+    if(blockHourText<currentHourText){
+        block = block.attr('class','row time-block past')
+    } else if(blockHourText===currentHourText){
+        block = block.attr('class','row time-block present')
+    } else if (blockHourText>currentHourText){
+        block = block.attr('class','row time-block future')
+    }
+}
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
