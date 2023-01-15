8627b5fcb34de332d41291ae7695675183cc5c6c
WJFil <greg.kitchen7@gmail.com>
2023-01-14 19:12:28 -0700

    save entered calendar events to local storage and have those events persist when page is refreshed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,24 +1,13 @@
 
-var calendarDate = $('#currentDay');
-var currentDate = dayjs().format('MMMM d, YYYY');
-calendarDate.text(currentDate);
-
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
 $(function () {
   var calendarDate = $('#currentDay');
   var currentDate = dayjs().format('MMMM d, YYYY');  
   var timeBlockArr=['09','10','11','12','13','14','15','16','17']
   var currentHour = dayjs().format('HH');
   var currentHourText = 'hour-'+currentHour;
+  var savedEventArr = JSON.parse(localStorage.getItem('savedResult')) || [];
 
-  //set date at top of calendar to current date
+  //set date in headerat top of calendar to current date
   calendarDate.text(currentDate);
 
   //loop through each time block element and set color based on if time block hour is before, equal to or after current hour
@@ -26,35 +15,31 @@ $(function () {
 
     var block = $('#hour-'+timeBlockArr[i]);
     var blockHourText = block.attr('id');
+    var blockTextArea = block.children('textarea');
+    var blockId = block.attr('id');
+    var blockPersist = localStorage.getItem(block.attr('id'));
+  0
+    //set time block events to existing value in local storage so that saved events persist when page is refreshed.
+    $(blockTextArea).val(blockPersist);
 
-
+    //compare hour of time block to current hour to determin if past, present, or future and set background color accordingly
     if(blockHourText<currentHourText){
         block = block.attr('class','row time-block past')
     } else if(blockHourText===currentHourText){
         block = block.attr('class','row time-block present')
     } else if (blockHourText>currentHourText){
         block = block.attr('class','row time-block future')
-    }
-}
+    }   
+  }
+  $("button").click(function(){
+    
+    var timeBlock = $(this).parent().attr('id');
+    var descrip = $(this).parent().children('textarea').val();    
+    
+    localStorage.setItem(timeBlock,descrip);
 
+  
+   
+  })
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
